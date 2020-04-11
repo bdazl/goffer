bc672949080e1bd7050a819d94334e4e61155df9
Jacob Peyron <jacob.peyron@gmail.com>
2020-04-11 16:05:07 +0200

    made paletted image available

--- cmd/fixpt/fixpt.go
+++ cmd/fixpt/fixpt.go
@@ -9,6 +9,14 @@ import (
 
 var (
 	output = "out/hello.png"
+
+	// https://colorhunt.co/palette/177866
+	Palette = color.Palette{
+		color.RGBA{0x20, 0x40, 0x51, 0xff},
+		color.RGBA{0x3B, 0x69, 0x78, 0xff},
+		color.RGBA{0x84, 0xA9, 0xAC, 0xff},
+		color.RGBA{0xCA, 0xE8, 0xD5, 0xff},
+	}
 )
 
 func main() {
@@ -16,12 +24,12 @@ func main() {
 	flag.Parse()
 
 	// Initialize the graphic context on an RGBA image
-	dest := image.NewRGBA(image.Rect(0, 0, 297, 210.0))
+	dest := image.NewRGBA(image.Rect(0, 0, 512, 512))
 	gc := draw2dimg.NewGraphicContext(dest)
 
 	// Set some properties
-	gc.SetFillColor(color.RGBA{0x44, 0xff, 0x44, 0xff})
-	gc.SetStrokeColor(color.RGBA{0x44, 0x44, 0x44, 0xff})
+	gc.SetFillColor(Palette[1])
+	gc.SetStrokeColor(Palette[2])
 	gc.SetLineWidth(2)
 
 	// Draw a closed shape
@@ -33,5 +41,18 @@ func main() {
 	gc.FillStroke()
 
 	// Save to file
-	draw2dimg.SaveToPngFile(output, dest)
+	draw2dimg.SaveToPngFile(output, toPaletted(dest, Palette))
+}
+
+func toPaletted(img image.Image, palette color.Palette) image.Image {
+	bnds := img.Bounds()
+	out := image.NewPaletted(bnds, palette)
+
+	for y := bnds.Min.Y; y < bnds.Max.Y; y++ {
+		for x := bnds.Min.X; x < bnds.Max.X; x++ {
+			idx := palette.Index(img.At(x, y))
+			out.SetColorIndex(x, y, uint8(idx))
+		}
+	}
+	return out
 }
