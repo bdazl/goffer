a13241ac229422b4a12e3f862ab02810106d6f12
Jacob Peyron <jacob.peyron@evolvetechnology.se>
2020-04-16 21:13:50 +0200

    expand palette

--- palette.go
+++ palette.go
@@ -5,7 +5,7 @@ import (
 )
 
 var (
-	Palette = Palette1
+	Palette = expandPalette(Palette1, 2)
 
 	// https://colorhunt.co/palette/177866
 	Palette1 = color.Palette{
@@ -15,3 +15,39 @@ var (
 		color.RGBA{0xCA, 0xE8, 0xD5, 0xff},
 	}
 )
+
+func expandPalette(p color.Palette, cnt int) color.Palette {
+	np := make(color.Palette, 0, len(p)*cnt*2*2*2)
+	for _, c := range p {
+		np = append(np, c)
+		for i := 0; i < cnt; i++ {
+			var m uint8 = uint8(i) * 5
+			np = append(np,
+				pNextCol(c, m, 0, 0),
+				pNextCol(c, m, m, 0),
+				pNextCol(c, m, 0, m),
+				pNextCol(c, 0, m, 0),
+				pNextCol(c, 0, m, m),
+				pNextCol(c, 0, 0, m),
+				pNextCol(c, m, m, m),
+
+				pNextCol(c, -m, 0, 0),
+				pNextCol(c, -m, -m, 0),
+				pNextCol(c, -m, 0, -m),
+				pNextCol(c, 0, -m, 0),
+				pNextCol(c, 0, -m, -m),
+				pNextCol(c, 0, 0, -m),
+				pNextCol(c, -m, -m, -m),
+			)
+		}
+	}
+	return np
+}
+
+func pNextCol(c color.Color, r, g, b uint8) color.Color {
+	rgba := c.(color.RGBA)
+	rgba.R = rgba.R + r
+	rgba.G = rgba.G + g
+	rgba.B = rgba.B + b
+	return rgba
+}
