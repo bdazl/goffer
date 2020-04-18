e89a1d0818e3eaa701678df2451daf76b898e530
Jacob Peyron <jacob.peyron@evolvetechnology.se>
2020-04-18 08:55:25 +0200

    image

--- pkg/image/draw.go
+++ pkg/image/draw.go
@@ -1,20 +1,22 @@
-package main
+package image
 
 import (
 	"image"
-	"image/color"
+
+	"github.com/HexHacks/goffer/pkg/global"
+	"github.com/HexHacks/goffer/pkg/palette"
 
 	"github.com/llgcode/draw2d/draw2dimg"
 )
 
-func drawCommon(palette color.Palette) (*image.RGBA, *draw2dimg.GraphicContext) {
-	bounds := image.Rect(0, 0, Width, Height)
+func New() (*image.RGBA, *draw2dimg.GraphicContext) {
+	bounds := image.Rect(0, 0, global.Width, global.Height)
 	img := image.NewRGBA(bounds)
 	gc := draw2dimg.NewGraphicContext(img)
 
 	// Set some properties
-	gc.SetFillColor(palette[1])
-	gc.SetStrokeColor(palette[2])
+	gc.SetFillColor(palette.Palette[1])
+	gc.SetStrokeColor(palette.Palette[2])
 	gc.SetLineWidth(2)
 
 	return img, gc
