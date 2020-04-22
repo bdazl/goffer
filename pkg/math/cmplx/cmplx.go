aeac5aa7d9ae437bd0a96a0593705321a54f1e74
Jacob Peyron <jacob.peyron@evolvetechnology.se>
2020-04-22 09:44:21 +0200

    math/cmplx ToVec

--- pkg/math/cmplx/cmplx.go
+++ pkg/math/cmplx/cmplx.go
@@ -1,5 +1,7 @@
 package cmplx
 
+import "gonum.org/v1/gonum/spatial/r2"
+
 // Complex functions
 // w: width of image
 // h: height of image
@@ -7,4 +9,11 @@ package cmplx
 // zh: height of complex region to stretch
 func ToImage(c complex128, w, h, zw, zh float64) (float64, float64) {
 	return (real(c) + zw) * w / (2.0 * zw), (-imag(c) + zh) * h / (2.0 * zh)
+}
+
+func ToVec(c complex128) r2.Vec {
+	return r2.Vec{
+		X: real(c),
+		Y: imag(c),
+	}
 }
