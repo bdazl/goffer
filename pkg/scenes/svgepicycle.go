2612345a4cda750de5e2a21979bd368c1e7b7a0a
Jacob Peyron <jacob.peyron@evolvetechnology.se>
2020-05-02 14:05:21 +0200

    svgepicycle: is now functioning!

--- pkg/scenes/svgepicycle.go
+++ pkg/scenes/svgepicycle.go
@@ -4,50 +4,52 @@ import (
 	"fmt"
 	"image"
 	"image/draw"
-	"math/cmplx"
 	"os"
 
 	"github.com/HexHacks/goffer/pkg/global"
-	jmath "github.com/HexHacks/goffer/pkg/math"
+	jimage "github.com/HexHacks/goffer/pkg/image"
+	"github.com/HexHacks/goffer/pkg/math/fourier"
 	"github.com/HexHacks/goffer/pkg/palette"
 	"github.com/HexHacks/goffer/pkg/svg"
-	"github.com/llgcode/draw2d/draw2dimg"
-	"gonum.org/v1/gonum/dsp/fourier"
-	"gonum.org/v1/gonum/spatial/r2"
 
-	jimage "github.com/HexHacks/goffer/pkg/image"
+	"github.com/llgcode/draw2d/draw2dimg"
+	"gonum.org/v1/gonum/spatial/r2"
 )
 
 type SvgEpicycle struct {
+	F          int
 	operations []svg.Operation
 	origPts    []r2.Vec
 	svgPts     []complex128
 	coeff      []complex128
-	fft        *fourier.CmplxFFT
 
-	// running the fourier version will leave us with some
-	// points that we want to draw lines through
-	pts []r2.Vec
+	pts []complex128
 }
 
 func (se *SvgEpicycle) Init() {
+	const (
+		perBezier = 100
+	)
 
 	fil, err := os.Open("assets/gubb_abs.svg")
 	panicOn(err)
 	defer fil.Close()
-
-	// out point when drawing
-	se.pts = make([]r2.Vec, 0, global.FrameCount)
 
 	s, err := svg.ParseSvg(fil)
 	panicOn(err)
 
 	se.operations = s.Groups[0].Paths[0].Operations
 	se.origPts = ExtractPoints(s)
-	se.svgPts = ExpandCurve(se.origPts, 100, 3.8)
+	se.svgPts = ExpandCurve(se.origPts, perBezier, 3.8)
+	se.coeff = fourier.Coefficients(se.svgPts, 20)
 
-	se.fft = fourier.NewCmplxFFT(len(se.svgPts))
-	se.coeff = se.fft.Coefficients(nil, se.svgPts)
+	loopCount := len(se.svgPts)
+	se.pts = make([]complex128, loopCount)
+	lcf := float64(loopCount)
+	for i := 0; i < loopCount; i++ {
+		fi := float64(i)
+		se.pts[i] = fourier.P(fi/lcf, se.coeff)
+	}
 }
 
 func ExpandCurve(curve []r2.Vec, perBezier int, scale float64) []complex128 {
@@ -124,37 +126,20 @@ func cBezier(t float64, a, b, c, d r2.Vec) r2.Vec {
 	return p0.Add(p1)
 }
 
-func (se *SvgEpicycle) InverseFTransform(s float64) r2.Vec {
-	out := 0 + 0i
-	w := complex(s*jmath.Tau/global.Total, 0)
-	for k, c := range se.coeff {
-		kk := complex(float64(k), 0)
-		freq := complex(se.fft.Freq(k), 0)
-
-		next := c * cmplx.Exp(w*freq*kk)
-		out = out + next
-	}
-
-	// scale
-	out = out / complex(float64(len(se.coeff)), 0)
-
-	return r2.Vec{
-		real(out),
-		imag(out),
-	}
-}
-
 func (se *SvgEpicycle) Frame(s float64) image.Image {
 	img, gc := jimage.New()
 	draw.Draw(img, img.Bounds(), &image.Uniform{palette.Palette[0]}, image.ZP, draw.Src)
 
 	// make new point and add it to list
-	v := se.InverseFTransform(s)
-	se.pts = append(se.pts, v)
+	//v := se.InverseFTransform(s)
+	//se.pts = append(se.pts, v)
 
 	//se.DrawOp(gc)
-	DrawLines(gc, CmplxSliceToVec(se.svgPts))
-	//DrawLines(gc, se.pts)
+	//DrawLines(gc, CmplxSliceToVec(se.svgPts))
+	perFrame := len(se.pts) / global.FrameCount
+	DrawCmplxLines(gc, se.pts, se.F*perFrame)
+
+	se.F++
 
 	return img
 }
@@ -193,6 +178,19 @@ func DrawLines(gc *draw2dimg.GraphicContext, pts []r2.Vec) {
 	gc.MoveTo(pts[0].X, pts[0].Y)
 	for i := 0; i < len(pts); i++ {
 		gc.LineTo(pts[i].X, pts[i].Y)
+	}
+	gc.Stroke()
+}
+
+func DrawCmplxLines(gc *draw2dimg.GraphicContext, pts []complex128, count int) {
+	if count >= len(pts) {
+		count = len(pts) - 1
+	}
+
+	// draw line through all points
+	gc.MoveTo(real(pts[0]), imag(pts[0]))
+	for i := 0; i < count; i++ {
+		gc.LineTo(real(pts[i]), imag(pts[i]))
 	}
 	gc.Stroke()
 }
