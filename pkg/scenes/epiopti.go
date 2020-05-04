6d0a557cc60691939e2b5a0b185b59a578faab68
Jacob Peyron <jacob.peyron@evolvetechnology.se>
2020-05-04 09:09:13 +0200

    small random epicycle experiment

--- pkg/scenes/epiopti.go
+++ pkg/scenes/epiopti.go
@@ -2,14 +2,30 @@ package scenes
 
 import (
 	"image"
+	"math/rand"
+
+	"github.com/HexHacks/goffer/pkg/animation/epicycles"
 )
 
 type EpiOpti struct {
+	epi *epicycles.Epicycles
 }
 
 func (e *EpiOpti) Init() {
+	const (
+		ptCnt = 200
+		cCnt  = 21
+	)
+
+	c := make([]complex128, cCnt)
+	for i := range c {
+		a, b := rand.Float64(), rand.Float64()
+		c[i] = complex(a*0.3-0.15, b*0.3-0.15)
+	}
+
+	e.epi = epicycles.New(c, ptCnt)
 }
 
 func (e *EpiOpti) Frame(t float64) image.Image {
-	return nil
+	return e.epi.Frame(t)
 }
