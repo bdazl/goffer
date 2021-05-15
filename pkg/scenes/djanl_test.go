dd718fa3b8771653d7698bdd380149c6083335ad
Jacob Peyron <jacob.peyron@evolvetechnology.se>
2021-05-15 10:03:04 +0200

    beat test

---
+++ pkg/scenes/djanl_test.go
@@ -0,0 +1,33 @@
+package scenes
+
+import (
+	"fmt"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+const (
+	eps = 2e-4
+)
+
+func TestBeatFunc(t *testing.T) {
+
+	tests := []struct {
+		in  float64
+		exp float64
+	}{
+		{0, 1.0},
+		{0.6024, 1.0},
+		{0.3, 0.0},
+		{0.9036, 0.0},
+		{1.205, 1.0},
+	}
+
+	for _, tst := range tests {
+		t.Run(fmt.Sprintf("g(%v)=%v", tst.in, tst.exp), func(nt *testing.T) {
+			out := beatFunc(tst.in)
+			assert.InDelta(t, tst.exp, out, eps)
+		})
+	}
+}
