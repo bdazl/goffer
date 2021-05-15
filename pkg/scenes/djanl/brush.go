3280960e3f3b4474e8430c64013892b84aa8fbef
Jacob Peyron <jacob.peyron@evolvetechnology.se>
2021-05-15 17:43:17 +0200

    djanl: center brush

--- pkg/scenes/djanl/brush.go
+++ pkg/scenes/djanl/brush.go
@@ -23,5 +23,14 @@ func newBrush(img image.Image) brush {
 }
 
 func (b *brush) Draw(onto draw.Image, dp image.Point) {
-	drawFullSrcMask(onto, b.img, &b.mask, dp)
+	var (
+		bnds = b.img.Bounds()
+		hx   = bnds.Size().X / 2
+		hy   = bnds.Size().Y / 2
+	)
+	ndp := image.Point{
+		X: dp.X - hx,
+		Y: dp.Y - hy,
+	}
+	drawFullSrcMask(onto, b.img, &b.mask, ndp)
 }
